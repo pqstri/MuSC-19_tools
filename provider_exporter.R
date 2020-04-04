0fc99c3b36fabcce0a7dc15b17b3540a2362e424
pqstri <lucacarmisciano@hotmail.com>
2020-04-04 16:09:55 +0200

    provider export

---
+++ provider_exporter.R
@@ -0,0 +1,48 @@
+# this script exports the content from text file by providers
+
+# replace the string with the path of the export file
+setwd("~/path_to_export_file.csv")
+
+# install (if needed) or load libreries
+if(!("tidyverse" %in% installed.packages()[,"Package"])) install.packages("tidyverse")
+require("tidyverse")
+
+# read file
+lines <- readr::read_csv("report.csv", skip = 4, col_names = "line")
+
+# indentify tables
+lines <- mutate(lines, sep_n = stringr::str_count(line, ";"))
+
+# name tables
+lines <- mutate(lines, table = ifelse(sep_n == 0, line, NA)) %>% tidyr::fill(table)
+lines <- filter(lines, table != line) %>% select(-sep_n)
+
+# separate tables
+tables <- split(lines, lines$table)
+
+# separate columns
+tables <- purrr::map(tables, ~ tidyr::separate(., line, sep = ";",
+  into = unlist(stringr::str_split(.$line[1], ";"))))
+
+# remove fake header lines
+tables <- purrr::map(tables, ~ filter(., row_number() > 1))
+
+# create a new data for converted files
+dir.create("tables", showWarnings = FALSE)
+
+# save tables
+purrr::map2(tables, names(tables), function(td, tn) {
+  names(td) <- stringr::str_replace_all(names(td), ";|,|-| |\\(|\\)", "_") %>% 
+    strtrim(40)
+  tn <- stringr::str_replace_all(tn, ";|,|-| ", "_")
+  fname <- paste("tables/", tn, ".sav", sep = "")
+  haven::write_sav(td, fname)
+  return("exported")
+  })
+
+
+
+
+
+
+
