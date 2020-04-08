7626215176e2f5b81ece835b9ad7c7366ea73f74
pqstri <lucacarmisciano@hotmail.com>
2020-04-09 01:56:34 +0200

    functions envelopment

--- patients_exporter.R
+++ patients_exporter.R
@@ -2,11 +2,11 @@
 # Set up
 #############################
 # this script exports the content from text file by providers
-exported_file_folder <- ""
+# exported_file_folder <- ""
 imported_file_path <- ""
 
 # replace the string with the path of the export file
-setwd(exported_file_folder)
+# setwd(exported_file_folder)
 
 # install (if needed) or load libreries
 if(!("tidyverse" %in% installed.packages()[,"Package"])) install.packages("tidyverse")
@@ -18,6 +18,8 @@ require("tidyverse")
 
 headers <- c("Provider;", ";;Demography", ";;MS history", ";;COVID 19 - ", 
              ";;COVID19 - ", ";;Comorbidity", ";;Surgery", ";;Comment")
+
+new_patient_tag <- "Provider;Patient;Sex;Created at;Updated at"
 
 MPS_format <- list(
   # dummy variables
@@ -166,151 +168,166 @@ MPS_format <- list(
 # Convert
 #############################
 
-# read file
-f <- data.frame(line = readr::read_lines(imported_file_path))
-f <- mutate(f, fields = stringr::str_count(line, ";"))
-
-# indentify patients
-new_patient_tag <- "Provider;Patient;Sex;Created at;Updated at"
-f <- mutate(f, pt = line == new_patient_tag)
-
-# name patients
-f <- mutate(f, pt = cumsum(pt)) %>%
-  filter(fields != 0)
-
-# indentify headers
-f <- mutate(f, is_header = map_lgl(line, ~ any(str_detect(., headers))))
-
-# identify multiple objects
-f <- f %>% group_by(pt) %>% 
-  mutate(block = cumsum(is_header)) %>% 
-  group_by(pt, block) %>% 
+convert <- function(file) {
   
-  # \n bug
-  mutate(newline = str_count(line, "\"") %% 2) %>% 
-  mutate(newline_open = cumsum(newline) %% 2) %>% 
-  mutate(newline = as.numeric(newline == 1 | newline_open == 1)) %>% 
-  group_by(pt, block, is_header, newline) %>% 
+  # read file
+  f <- data.frame(line = readr::read_lines(file))
+  f <- mutate(f, fields = stringr::str_count(line, ";"))
   
-  # continue
-  group_by(pt, block) %>% 
-  mutate(sheets = n()) %>% 
+  # indentify patients
+  f <- mutate(f, pt = line == new_patient_tag)
   
-  mutate(newline_penalty = ifelse(is_header & any(newline), sum(newline)-1, 0)) %>%
-  group_by(pt, block) %>%
-  mutate(multiplier = ifelse(is_header & sheets > 2, sheets-1-newline_penalty, 1)) %>%
+  # name patients
+  f <- mutate(f, pt = cumsum(pt)) %>%
+    filter(fields != 0)
   
-  uncount(multiplier) %>% 
-  ungroup()
-
-# condensate info
-f <- group_by(f, pt, is_header) %>%
-  summarise(line = paste(line, collapse = ";"),
-            fields = sum(fields))
-
-# integrity check
-f %>% group_by(pt) %>% summarise(ok = all(fields == fields[1])) %>% 
-  filter(!ok) %>% .$pt
-
-# fix the ";" bug
-reimport <- function(line) {
-  cat("--> handling errors on: ", line, "\n\n")
-  read.csv2(text = line, header = FALSE) %>% 
-    gather("k", "v") %>% 
-    mutate(v = ifelse(is.na(v), "", v)) %>% 
-    mutate(v = str_replace_all(v, ";", ",")) %>% 
-    {paste(.$v, collapse = ";")}
+  # indentify headers
+  f <- mutate(f, is_header = map_lgl(line, ~ any(str_detect(., headers))))
+  
+  # identify multiple objects
+  f <- f %>% group_by(pt) %>% 
+    mutate(block = cumsum(is_header)) %>% 
+    group_by(pt, block) %>% 
+    
+    # \n bug
+    mutate(newline = str_count(line, "\"") %% 2) %>% 
+    mutate(newline_open = cumsum(newline) %% 2) %>% 
+    mutate(newline = as.numeric(newline == 1 | newline_open == 1)) %>% 
+    group_by(pt, block, is_header, newline) %>% 
+    
+    # continue
+    group_by(pt, block) %>% 
+    mutate(sheets = n()) %>% 
+    
+    mutate(newline_penalty = ifelse(is_header & any(newline), sum(newline)-1, 0)) %>%
+    group_by(pt, block) %>%
+    mutate(multiplier = ifelse(is_header & sheets > 2, sheets-1-newline_penalty, 1)) %>%
+    
+    uncount(multiplier) %>% 
+    ungroup()
+  
+  # condensate info
+  f <- group_by(f, pt, is_header) %>%
+    summarise(line = paste(line, collapse = ";"),
+              fields = sum(fields))
+  
+  # integrity check
+  f %>% group_by(pt) %>% summarise(ok = all(fields == fields[1])) %>% 
+    filter(!ok) %>% .$pt
+  
+  # fix the ";" bug
+  reimport <- function(line) {
+    cat("--> handling errors on: ", line, "\n\n")
+    read.csv2(text = line, header = FALSE) %>% 
+      gather("k", "v") %>% 
+      mutate(v = ifelse(is.na(v), "", v)) %>% 
+      mutate(v = str_replace_all(v, ";", ",")) %>% 
+      {paste(.$v, collapse = ";")}
+  }
+  
+  f <- f %>%
+    rowwise() %>% 
+    mutate(line = ifelse(grepl("\"", line), reimport(line), line)) %>% 
+    mutate(fields = str_count(line, ";")) %>% 
+    ungroup()
+  
+  # integrity check
+  f %>% group_by(pt) %>% summarise(ok = all(fields == fields[1])) %>% 
+    filter(!ok) %>% .$pt
+  
+  # tokenization and verticalization
+  max_length <- 300
+  f <- f %>%
+    mutate(ambiguous_separator = ifelse(grepl("\".*\"", line), 0, 1)) %>% 
+    separate(line, as.character(1:max_length), sep = ";") %>% 
+    tidyr::gather("position", "value", as.character(1:max_length), convert = TRUE) %>%
+    group_by(pt, position) %>% 
+    summarise(value = paste(value, collapse = "!!!")) %>% 
+    tidyr::separate(value, c("value", "header"), sep = "!!!") %>% 
+    filter(header != "NA", !is.na(header)) %>%
+    arrange(pt, position)
+  
+  # include topic in variable name
+  f <- f %>% 
+    group_by(pt) %>% 
+    mutate(blank = ifelse(value == "" & header == "", 1, 0)) %>% 
+    mutate(prev_blank = lag(blank)) %>% 
+    mutate(title = lag(blank == 1 & prev_blank == 1)) %>% 
+    mutate(title = ifelse(title, header, NA)) %>% 
+    filter(!blank) %>% 
+    fill(title) %>% 
+    mutate(title = ifelse(is.na(title), "Base", title)) %>%
+    select(-blank, -prev_blank) %>% 
+    ungroup()
+  
+  # avoid duplicates and horizontalize
+  f <- f %>%
+    tidyr::unite("variable", title, header) %>% 
+    group_by(pt, variable) %>%
+    mutate(multiple = row_number()) %>% 
+    ungroup() %>% 
+    mutate(variable = ifelse(multiple > 1, paste(variable, multiple, sep = "_"), variable)) %>%
+    select(-position, -multiple) %>% 
+    tidyr::spread(variable, value)
+  
+  return(f)
 }
 
-f <- f %>%
-  rowwise() %>% 
-  mutate(line = ifelse(grepl("\"", line), reimport(line), line)) %>% 
-  mutate(fields = str_count(line, ";")) %>% 
-  ungroup()
+#############################
+# Clean
+#############################
 
-# integrity check
-f %>% group_by(pt) %>% summarise(ok = all(fields == fields[1])) %>% 
-  filter(!ok) %>% .$pt
+clean <- function(data) {
 
-# tokenization and verticalization
-max_length <- 300
-f <- f2 %>%
-  mutate(ambiguous_separator = ifelse(grepl("\".*\"", line), 0, 1)) %>% 
-  separate(line, as.character(1:max_length), sep = ";") %>% 
-  tidyr::gather("position", "value", as.character(1:max_length), convert = TRUE) %>%
-  group_by(pt, position) %>% 
-  summarise(value = paste(value, collapse = "!!!")) %>% 
-  tidyr::separate(value, c("value", "header"), sep = "!!!") %>% 
-  filter(header != "NA", !is.na(header)) %>%
-  arrange(pt, position)
-
-# include topic in variable name
-f <- f %>% 
-  group_by(pt) %>% 
-  mutate(blank = ifelse(value == "" & header == "", 1, 0)) %>% 
-  mutate(prev_blank = lag(blank)) %>% 
-  mutate(title = lag(blank == 1 & prev_blank == 1)) %>% 
-  mutate(title = ifelse(title, header, NA)) %>% 
-  filter(!blank) %>% 
-  fill(title) %>% 
-  mutate(title = ifelse(is.na(title), "Base", title)) %>%
-  select(-blank, -prev_blank) %>% 
-  ungroup()
-
-# avoid duplicates and horizontalize
-f <- f %>%
-  tidyr::unite("variable", title, header) %>% 
-  group_by(pt, variable) %>%
-  mutate(multiple = row_number()) %>% 
-  ungroup() %>% 
-  mutate(variable = ifelse(multiple > 1, paste(variable, multiple, sep = "_"), variable)) %>%
-  select(-position, -multiple) %>% 
-  tidyr::spread(variable, value)
-
-# patient ID
-f <- mutate(f, upid = sprintf("%s-%s-%s", Demography_Country, 
-                              `Demography_Site Code`, `Demography_Patient Code`))
-
-# dummification
-# COVID 19 - Sign and symptom_Other symptoms
-f <- separate(f, `COVID 19 - Sign and symptom_Other symptoms`,
-         into = as.character(1:10), sep = ",") %>% 
-  select(upid, as.character(1:10)) %>% 
-  gather("position", "symptom", -upid) %>% 
-  filter(!is.na(symptom) & symptom != "") %>% 
-  mutate(symptom = trimws(symptom)) %>% 
-  mutate(symptom = paste("COVID 19 - Sign and symptom_Other symptoms", symptom, sep = "_")) %>% 
-  mutate(position = 1) %>% 
-  spread(symptom, position) %>% 
-  right_join(f) %>% 
-  mutate_at(vars(contains("COVID 19 - Sign and symptom_Other symptoms")), ~ replace_na(., 0))
-
-# COVID 19 - Sign and symptom_Signs of infection
-f <- separate(f, `COVID 19 - Sign and symptom_Signs of infection`,
-              into = as.character(1:10), sep = ",") %>% 
-  select(upid, as.character(1:10)) %>% 
-  gather("position", "symptom", -upid) %>% 
-  filter(!is.na(symptom) & symptom != "") %>% 
-  mutate(symptom = trimws(symptom)) %>% 
-  mutate(symptom = paste("COVID 19 - Sign and symptom_Signs of infection", symptom, sep = "_")) %>% 
-  mutate(position = 1) %>% 
-  spread(symptom, position) %>% 
-  right_join(f) %>% 
-  mutate_at(vars(contains("COVID 19 - Sign and symptom_Signs of infection")), ~ replace_na(., 0))
-
-# COVID19 - Diagnosis, Treatment_Treatments
-f <- separate(f, `COVID19 - Diagnosis, Treatment_Treatments`,
-              into = as.character(1:10), sep = ",") %>% 
-  select(upid, as.character(1:10)) %>% 
-  gather("position", "choice", -upid) %>% 
-  filter(!is.na(choice) & choice != "") %>% 
-  mutate(choice = trimws(choice)) %>% 
-  mutate(choice = paste("COVID19 - Diagnosis, Treatment_Treatments", choice, sep = "_")) %>% 
-  mutate(position = 1) %>% 
-  spread(choice, position) %>% 
-  right_join(f) %>% 
-  mutate_at(vars(contains("COVID19 - Diagnosis, Treatment_Treatments")), ~ replace_na(., 0))
-
+  # leading zeros on Site Code
+  f <- mutate(data, `Demography_Site Code` = str_pad(`Demography_Site Code`, 2, pad = "0"))
+  
+  # patient ID
+  f <- mutate(f, upid = sprintf("%s-%s-%s", Demography_Country, 
+                                `Demography_Site Code`, `Demography_Patient Code`))
+  
+  # dummification
+  # COVID 19 - Sign and symptom_Other symptoms
+  f <- separate(f, `COVID 19 - Sign and symptom_Other symptoms`,
+           into = as.character(1:10), sep = ",") %>% 
+    select(upid, as.character(1:10)) %>% 
+    gather("position", "symptom", -upid) %>% 
+    filter(!is.na(symptom) & symptom != "") %>% 
+    mutate(symptom = trimws(symptom)) %>% 
+    mutate(symptom = paste("COVID 19 - Sign and symptom_Other symptoms", symptom, sep = "_")) %>% 
+    mutate(position = 1) %>% 
+    spread(symptom, position) %>% 
+    right_join(f) %>% 
+    mutate_at(vars(contains("COVID 19 - Sign and symptom_Other symptoms")), ~ replace_na(., 0))
+  
+  # COVID 19 - Sign and symptom_Signs of infection
+  f <- separate(f, `COVID 19 - Sign and symptom_Signs of infection`,
+                into = as.character(1:10), sep = ",") %>% 
+    select(upid, as.character(1:10)) %>% 
+    gather("position", "symptom", -upid) %>% 
+    filter(!is.na(symptom) & symptom != "") %>% 
+    mutate(symptom = trimws(symptom)) %>% 
+    mutate(symptom = paste("COVID 19 - Sign and symptom_Signs of infection", symptom, sep = "_")) %>% 
+    mutate(position = 1) %>% 
+    spread(symptom, position) %>% 
+    right_join(f) %>% 
+    mutate_at(vars(contains("COVID 19 - Sign and symptom_Signs of infection")), ~ replace_na(., 0))
+  
+  # COVID19 - Diagnosis, Treatment_Treatments
+  f <- separate(f, `COVID19 - Diagnosis, Treatment_Treatments`,
+                into = as.character(1:10), sep = ",") %>% 
+    select(upid, as.character(1:10)) %>% 
+    gather("position", "choice", -upid) %>% 
+    filter(!is.na(choice) & choice != "") %>% 
+    mutate(choice = trimws(choice)) %>% 
+    mutate(choice = paste("COVID19 - Diagnosis, Treatment_Treatments", choice, sep = "_")) %>% 
+    mutate(position = 1) %>% 
+    spread(choice, position) %>% 
+    right_join(f) %>% 
+    mutate_at(vars(contains("COVID19 - Diagnosis, Treatment_Treatments")), ~ replace_na(., 0))
+  
+  return(f)
+}
 
 # clean name for spss
 # names(f) <- stringr::str_replace_all(names(f), ";|,|-| |\\(|\\)|\\.|\\\\|\\/|\\+", "_") %>% 
@@ -318,29 +335,56 @@ f <- separate(f, `COVID19 - Diagnosis, Treatment_Treatments`,
 # f <- mutate_all(f, ~ stringr::str_replace_all(., ";|,|-| |:|/", "_"))
 
 
-
 #############################
 # Export
 #############################
 
-# name of the exported file
-fname <- paste("musc19", format(Sys.time(), "_%d%b%Y"), ".sav", sep = "")
+prepare_export <- function(f) {
+  # name of the exported file
+  fname <- paste("musc19", format(Sys.time(), "_%d%b%Y"), ".sav", sep = "")
+  
+  # fit destination format
+  selected_col <- unname(MPS_format$var_dictionary) %>% na.omit()
+  selected_col <- intersect(selected_col, names(f))
+  col_index <- which(MPS_format$var_dictionary %in% names(f))
+  
+  # rename columns and take white list
+  export <- f %>% 
+    filter(!(Base_Provider %in% MPS_format$provider_blacklist)) %>%
+    select(selected_col) %>% 
+    rename(MPS_format$var_dictionary[col_index])
+  
+  return(export)
+}
 
-# fit destination format
-selected_col <- unname(MPS_format$var_dictionary) %>% na.omit()
-selected_col <- intersect(selected_col, names(f))
-col_index <- which(MPS_format$var_dictionary %in% names(f))
+# export <- function() {
+#  
+#   # rename columns and take white list
+#   export <- f %>% 
+#     filter(!(Base_Provider %in% MPS_format$provider_blacklist)) %>%
+#     select(selected_col) %>% 
+#     rename(MPS_format$var_dictionary[col_index])
+#   
+#   return(export)
+# }
 
-# rename columns
-export <- f %>% 
-  filter(!(Base_Provider %in% MPS_format$provider_blacklist)) %>%
-  select(selected_col) %>% 
-  rename(MPS_format$var_dictionary[col_index])
-
-
+# export_bl <- function(f) {
+#   
+#   # rename columns and take black list
+#   internal <- f %>% 
+#     filter(Base_Provider %in% MPS_format$provider_blacklist) %>%
+#     filter(`Demography_Site Code` != "00") %>%
+#     select(selected_col) %>% 
+#     rename(MPS_format$var_dictionary[col_index])
+#   
+#   return(internal)
+# }
+# type fix
+# write_csv(export, "temp.csv")
+# export2 <- read_csv("temp.csv", col_types = )
 
 # save spss output
-haven::write_sav(export, fname)
+# haven::write_sav(export, fname)
 
 
 
