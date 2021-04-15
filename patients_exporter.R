11ef50a9ebc61153a5668a725f394e667cb3cb87
pqstri <lucacarmisciano@hotmail.com>
2021-04-15 19:16:11 +0200

    bug on bugfix

--- patients_exporter.R
+++ patients_exporter.R
@@ -246,7 +246,7 @@ convert <- function(file) {
   f[f$line == fixed,]$fields <- stringr::str_count(fixed, ";")
   
   # Di Sapio offline
-  f <- f[-which(f$line == ";;;;;;;29/02/1984;Secondary progressive MS (SPMS);7.5;01/11/2020;No;;;;;;No;;;Yes;Interferon;;;;;;;;;;13/03/2021 12:47;13/03/2021 12:47"),]
+  # f <- f[-which(f$line == ";;;;;;;29/02/1984;Secondary progressive MS (SPMS);7.5;01/11/2020;No;;;;;;No;;;Yes;Interferon;;;;;;;;;;13/03/2021 12:47;13/03/2021 12:47"),]
   
   # name patients
   f <- mutate(f, pt = cumsum(pt)) %>%
